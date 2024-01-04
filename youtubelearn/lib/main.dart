3a559c14495633cb91fa200e5e055217950d1f9b
Nidham Kacha <nidham78@gmail.com>
2024-01-04 12:49:17 +0530

    3-1-24
    Signed-off-by: Nidham Kacha <nidham78@gmail.com>

--- youtubelearn/lib/main.dart
+++ youtubelearn/lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     home: MyApp(),
   ));
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,7 +13,149 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my portfolioapp'),
+        title: Text('My Portfolio App'),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.only(top: 100.0, left: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 45,
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Nidham Kacha",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Flutter Developer",
+                      style: TextStyle(fontSize: 18),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.only(left: 22.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.school,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text(
+                        "Skill Qode",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.computer,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text(
+                        "Skill Qode",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_pin,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text(
+                        "Skill Qode",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.email_rounded,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text(
+                        "Skill Qode",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Text(
+                        "Skill Qode",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "About Me I Am Nidham Kacha And Now I Am Doing Flutter Development At Skill Qode This Is My First Portfolio App.",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            Text(
+              "Created By - Nid's",
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
       ),
     );
   }
