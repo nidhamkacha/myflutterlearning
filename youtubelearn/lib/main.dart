6bf1db6b98367503c1cc3cd034559dc806d90b5c
Nidham Kacha <nidham78@gmail.com>
2024-01-04 10:32:17 +0530

    4-1-24
    Signed-off-by: Nidham Kacha <nidham78@gmail.com>

--- youtubelearn/lib/main.dart
+++ youtubelearn/lib/main.dart
@@ -1,53 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Sample App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my portfolioapp'),
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Sample App'),
-            backgroundColor: Colors.blue, // Set the app bar color here
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.black,
-              ),
-              Container(
-                height: 150,
-                width: 100,
-                color: const Color.fromARGB(255, 108, 16, 16),
-              ),
-              Container(
-                height: 150,
-                width: 100,
-                color: Color.fromARGB(255, 7, 222, 158),
-              ),
-              Container(
-                height: 150,
-                width: 100,
-                color: Color.fromARGB(255, 163, 234, 20),
-              ),
-              Container(
-                height: 150,
-                width: 100,
-                color: Color.fromARGB(255, 37, 16, 170),
-              ),
-            ],
-          )),
     );
   }
 }
